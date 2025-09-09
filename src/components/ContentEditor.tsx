2733b554ae995380c1abdcd960e31d9eea82a0cd
Nick <njthrockmorton@gmail.com>
2025-09-09 15:49:01 -0500

    Add React import to ContentEditor component
    - Import React explicitly to resolve JSX namespace issues
    - Ensures proper TypeScript recognition of React.JSX.Element types

--- src/components/ContentEditor.tsx
+++ src/components/ContentEditor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { XMarkIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 
 interface ContentEditorProps {
